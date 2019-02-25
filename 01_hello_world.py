41a3ff214750b2108b51cc8d824d56fc902b0c95
WGC575 <wgc575@gmail.com>
2019-02-25 14:29:28 -0800

    First commit, basic syntax with python.

---
+++ 01_hello_world.py
@@ -0,0 +1,8 @@
+class my_class(object):
+    print("Hello World!")
+    #pass is used to keep integrity of code, doing nothing. 
+    #Its usage is like "continue" in C++ but it could also be applied to classes and functions.
+    pass
+
+
+
---
+++ 02_variable.py
@@ -0,0 +1,8 @@
+carname = "Volvo"
+print(carname)
+
+x = 5
+y = 10
+z = x + y
+print(x, y, z, sep = ', ')
+
