8be57e9230a869311738b1e63c4a3eed4c5420f0
WGC575 <wgc575@gmail.com>
2019-02-26 01:29:58 -0800

    Update up to dictionary

---
+++ 09_set.py
@@ -0,0 +1,9 @@
+#Set is a collection that is unordered and unindexed, using {} instead of()
+
+#Construction
+set_new = set((1,2,3))
+print(set_new)
+
+#methods concerning set is similar to tuple
+
+pass
