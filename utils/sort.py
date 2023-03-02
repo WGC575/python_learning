ec7ec1eae2c17ae172fa01b0f6c338c20eb0e6f0
WGC575 <wgc575@gmail.com>
2023-03-01 22:07:01 -0800

    [v202303012206] Merge sort util.

--- utils/sort.py
+++ utils/sort.py
@@ -20,10 +20,39 @@ def bubble_sort(arr):
 
 def merge_sort(arr):
     # base case
-    if len(arr) < 1:
+    if len(arr) <= 1:
         return arr
     
     # sort halves recursively
     mid = len(arr) // 2 # floor
     left_half = arr[:mid]
     right_half = arr[mid:]
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    i = j = k = 0
+    while i < len(left_half) and j < len(right_half):
+        if left_half[i] < right_half[j]:
+            arr[k] = left_half[i]
+            i += 1
+        else:
+            arr[k] = right_half[j]
+            j += 1
+        k += 1
+
+    # dealing with the last element
+    while i < len(left_half):
+        arr[k] = left_half[i]
+        i += 1
+        k += 1
+    while j < len(left_half):
+        arr[k] = right_half[j]
+        j += 1
+        k += 1
+    return arr
+
+# example usage
+
+arr = [5, 2, 8, 3, 1, 6]
+sorted_arr = merge_sort(arr)
+print(sorted_arr)
